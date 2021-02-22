a86c43232373e223f3760f5665e5af13111723a7
GUILHERME GARCIA DOS SANTOS <gg.dossantos@hotmail.com>
2021-02-22 15:37:58 -0300

    Ajustando configurações para utilizar jest com typescript

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,6 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/en/configuration.html
- */
-
 export default {  
   clearMocks: true,
   coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  coverageProvider: "babel",
   testEnvironment: "node",
 };
