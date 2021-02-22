d76b3182d567ff69650208467c180b0e666a6d09
GUILHERME GARCIA DOS SANTOS <gg.dossantos@hotmail.com>
2021-02-22 17:28:59 -0300

    Criando configuração do postgresql

---
+++ src/configuration/database/pg/PgPoolConnection.spec.ts
@@ -0,0 +1,17 @@
+import { PgPoolConnection } from './PgPoolConnection'
+import { Pool } from 'pg'
+
+describe('PgPoolConnection', (): void => {
+    
+    test('should create a pg pool', async (): Promise<void> => {        
+        const sut = PgPoolConnection.getInstance()
+        expect(sut.pool).toBeInstanceOf(Pool)
+    })
+
+    test('should return an existing pool if exists', async (): Promise<void> => {        
+        const sutA = PgPoolConnection.getInstance();
+        const sutB = PgPoolConnection.getInstance();
+        
+        expect(sutA.pool).toBe(sutB.pool);
+    });
+});
---
+++ src/configuration/database/pg/PgPoolConnection.ts
@@ -0,0 +1,27 @@
+import { Pool } from 'pg';
+
+export class PgPoolConnection {
+    private static instance: PgPoolConnection;
+    pool: Pool;
+
+    private constructor () {
+        this.pool = new Pool();
+    }
+
+    public static getInstance(): PgPoolConnection {
+        if (!PgPoolConnection.instance) {
+            PgPoolConnection.instance = new PgPoolConnection();
+            // TODO: Improve the connection. Get from env
+            PgPoolConnection.instance.pool = new Pool({
+                host: '127.0.0.1',
+                port: 5432,
+                user: 'postgres',
+                database: 'postgres',
+                password: 'postgres'
+            });            
+        }
+
+        return PgPoolConnection.instance;
+    }
+
+}
