2ae0aaed9018b38d39b12901fbe330561f2b325a
GUILHERME GARCIA DOS SANTOS <gg.dossantos@hotmail.com>
2021-02-23 18:28:05 -0300

    Aumenta tamanho da string retornada pelo IdGenerator para o máximo permitido

--- src/core/usecase/helper/IdGenerator.ts
+++ src/core/usecase/helper/IdGenerator.ts
@@ -2,7 +2,7 @@ import { IdGeneratorService } from "../service/IdGeneratorService";
 
 export class IdGenerator implements IdGeneratorService {
     generate(): string {
-        return Math.random().toString(36).substring(2, 10)
+        return Math.random().toString(36).substring(2, 12)
     }
 
 }
