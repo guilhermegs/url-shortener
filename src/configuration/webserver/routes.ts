fbe642b1fdeaf30b3d41006dd4cc948782cd2841
GUILHERME GARCIA DOS SANTOS <gg.dossantos@hotmail.com>
2021-02-24 00:49:57 -0300

    Adicionando swagger na aplicação

--- src/configuration/webserver/routes.ts
+++ src/configuration/webserver/routes.ts
@@ -1,9 +1,17 @@
+
 import { Router } from 'express'
+import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs'
 import { ShortenUrlRouterComposer } from './composer/ShortenUrlRouterComposer'
 import { RouterAdapter } from './adapter'
 import { RedirectUrlRouterComposer } from './composer/RedirectUrlRouterComposer'
 
+const swaggerDocument = YAML.load('src/configuration/swagger/swagger.yaml');
+
 const router = Router()
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.post(
     '/encurtador', 
