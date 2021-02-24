349faa080b32c9fe268a79f1e237fbe5c86304cb
GUILHERME GARCIA DOS SANTOS <gg.dossantos@hotmail.com>
2021-02-24 00:04:11 -0300

    Cria nova rota para redirecionamento

--- src/configuration/webserver/routes.ts
+++ src/configuration/webserver/routes.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express'
 import { ShortenUrlRouterComposer } from './composer/ShortenUrlRouterComposer'
 import { RouterAdapter } from './adapter'
+import { RedirectUrlRouterComposer } from './composer/RedirectUrlRouterComposer'
 
 const router = Router()
 
 router.post(
     '/encurtador', 
     RouterAdapter.adapt(ShortenUrlRouterComposer.compose())
+)
+
+router.get(
+    '/:newUrl', 
+    RouterAdapter.adapt(RedirectUrlRouterComposer.compose())
 )
 
 export { router }
