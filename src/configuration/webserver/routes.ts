de83527310d1f95501b69c9c195edaea885e68d1
GUILHERME GARCIA DOS SANTOS <gg.dossantos@hotmail.com>
2021-02-24 03:41:22 -0300

    Alterando host do swagger de acordo com o host do request

--- src/configuration/webserver/routes.ts
+++ src/configuration/webserver/routes.ts
@@ -10,8 +10,12 @@ const swaggerDocument = YAML.load('src/configuration/swagger/swagger.yaml');
 
 const router = Router()
 
-router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.use('/api-docs', function(req, res, next){
+    swaggerDocument.host = req.get('host');
+    req.swaggerDoc = swaggerDocument;
+    next();
+}, swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup());
 
 router.post(
     '/encurtador', 
