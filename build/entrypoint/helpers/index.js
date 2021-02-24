04b825a16ccfd8e6c1ec7943c3f489727f2524c4
GUILHERME GARCIA DOS SANTOS <gg.dossantos@hotmail.com>
2021-02-24 03:52:05 -0300

    Adicionando build no gitignore para teste

--- build/entrypoint/helpers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HttpResponse = exports.HttpRequest = void 0;
-var HttpRequest_1 = require("./HttpRequest");
-Object.defineProperty(exports, "HttpRequest", { enumerable: true, get: function () { return HttpRequest_1.HttpRequest; } });
-var HttpResponse_1 = require("./HttpResponse");
-Object.defineProperty(exports, "HttpResponse", { enumerable: true, get: function () { return HttpResponse_1.HttpResponse; } });
--- build/provider/database/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UrlService = void 0;
-var UrlDatabaseProvider_1 = require("./UrlDatabaseProvider");
-Object.defineProperty(exports, "UrlService", { enumerable: true, get: function () { return UrlDatabaseProvider_1.UrlDatabaseProvider; } });
