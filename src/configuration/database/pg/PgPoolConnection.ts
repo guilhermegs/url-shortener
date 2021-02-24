bd44e349581ba160502dd936869bb144e94c4b4c
GUILHERME GARCIA DOS SANTOS <gg.dossantos@hotmail.com>
2021-02-24 02:21:09 -0300

    Removendo console.log

--- src/configuration/database/pg/PgPoolConnection.ts
+++ src/configuration/database/pg/PgPoolConnection.ts
@@ -14,9 +14,7 @@ export class PgPoolConnection {
     public static getInstance(): PgPoolConnection {
         if (!PgPoolConnection.instance) {
             const isProduction = process.env.NODE_ENV === 'production'
-            const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-
-            console.log(connectionString)
+            const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`            
 
             PgPoolConnection.instance = new PgPoolConnection();
             PgPoolConnection.instance.pool = new Pool({
